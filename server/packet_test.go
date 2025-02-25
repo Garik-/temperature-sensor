b5b9f00704c02f033c09417e65158c67f88b18b1
Gar|k <garik.djan@gmail.com>
2025-02-26 01:25:15 +0300

    fix: packet test

--- server/packet_test.go
+++ server/packet_test.go
@@ -2,33 +2,32 @@ package main
 
 import (
 	"encoding/binary"
+	"math"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestEncodePacket(t *testing.T) {
-	data := make([]byte, 12)
+	// Создаем структуру
+	exp := Packet{
+		Temperature: 25.5,
+		Humidity:    60.0,
+		Pressure:    101325.0,
+	}
 
-	// 3 float32 по 4 байта каждый
-	binary.LittleEndian.PutUint32(
-		data[0:4],
-		binary.LittleEndian.Uint32([]byte{0x00, 0x00, 0xCC, 0x41})) // 25.5
-	binary.LittleEndian.PutUint32(
-		data[4:8],
-		binary.LittleEndian.Uint32([]byte{0x00, 0x00, 0x70, 0x42})) // 60.0
-	binary.LittleEndian.PutUint32(
-		data[8:12],
-		binary.LittleEndian.Uint32([]byte{0x00, 0x40, 0x9C, 0x47})) // 101325.0 (600 мм рт.ст. в Паскалях)
+	// Преобразуем структуру в байты (Little Endian)
+	bytes := make([]byte, 12) // 3 поля по 4 байта (float32)
+	binary.LittleEndian.PutUint32(bytes[0:4], math.Float32bits(exp.Temperature))
+	binary.LittleEndian.PutUint32(bytes[4:8], math.Float32bits(exp.Humidity))
+	binary.LittleEndian.PutUint32(bytes[8:12], math.Float32bits(exp.Pressure))
 
 	var packet Packet
-	err := encodePacket(data, &packet)
+	err := encodePacket(bytes, &packet)
 	require.NoError(t, err)
 
-	assert.Equal(t, float32(25.5), packet.Temperature)
-	assert.Equal(t, float32(60.0), packet.Humidity)
-	assert.InDelta(t, 600.0, packet.Pressure, 0.1) // Проверка с учетом погрешности
-	assert.InEpsilon(t, time.Now(), packet.timestamp, float64(time.Second))
+	assert.InEpsilon(t, exp.Temperature, packet.Temperature, 1e-6)
+	assert.InEpsilon(t, exp.Humidity, packet.Humidity, 1e-6)
+	assert.InEpsilon(t, exp.Pressure/133.322, packet.Pressure, 1e-6)
 }
