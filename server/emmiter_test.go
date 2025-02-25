5ef0436741055140b50cf33e6b30b451d9b2b9a0
Gar|k <garik.djan@gmail.com>
2025-02-26 00:23:21 +0300

    feat: add some tests

---
+++ server/emmiter_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEventEmitter(t *testing.T) {
+	t.Run("Subscribe and Emit", func(t *testing.T) {
+		emitter := NewEventEmitter()
+		ch := emitter.Subscribe()
+		defer emitter.Unsubscribe(ch)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		done := make(chan struct{})
+
+		exp := Packet{
+			Temperature: 25.5,
+			Humidity:    60.0,
+			Pressure:    760.0,
+		}
+
+		go func() {
+			defer wg.Done()
+
+			close(done)
+
+			packet := <-ch
+			assert.Equal(t, exp.Temperature, packet.Temperature)
+			assert.Equal(t, exp.Humidity, packet.Humidity)
+			assert.Equal(t, exp.Pressure, packet.Pressure)
+		}()
+
+		<-done
+		emitter.Emit(exp)
+
+		wg.Wait()
+	})
+
+	t.Run("Unsubscribe", func(t *testing.T) {
+		emitter := NewEventEmitter()
+		ch := emitter.Subscribe()
+
+		emitter.Unsubscribe(ch)
+
+		_, ok := <-ch
+		assert.False(t, ok)
+	})
+
+	t.Run("Close", func(t *testing.T) {
+		emitter := NewEventEmitter()
+		ch1 := emitter.Subscribe()
+		ch2 := emitter.Subscribe()
+
+		emitter.Close()
+
+		_, ok1 := <-ch1
+		assert.False(t, ok1)
+
+		_, ok2 := <-ch2
+		assert.False(t, ok2)
+
+		assert.Nil(t, emitter.subscribers)
+	})
+}
---
+++ server/packet_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/binary"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncodePacket(t *testing.T) {
+	data := make([]byte, 12)
+
+	// 3 float32 по 4 байта каждый
+	binary.LittleEndian.PutUint32(data[0:4], binary.LittleEndian.Uint32([]byte{0x00, 0x00, 0xCC, 0x41}))  // 25.5
+	binary.LittleEndian.PutUint32(data[4:8], binary.LittleEndian.Uint32([]byte{0x00, 0x00, 0x70, 0x42}))  // 60.0
+	binary.LittleEndian.PutUint32(data[8:12], binary.LittleEndian.Uint32([]byte{0x00, 0x40, 0x9C, 0x47})) // 101325.0 (600 мм рт.ст. в Паскалях)
+
+	var packet Packet
+	err := encodePacket(data, &packet)
+	assert.NoError(t, err)
+
+	assert.Equal(t, float32(25.5), packet.Temperature)
+	assert.Equal(t, float32(60.0), packet.Humidity)
+	assert.InDelta(t, 600.0, packet.Pressure, 0.1) // Проверка с учетом погрешности
+	assert.WithinDuration(t, time.Now(), packet.timestamp, time.Second)
+}
