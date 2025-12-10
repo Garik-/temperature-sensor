6ba43edbe992393f2a40d1bbc5d4eee044639e31
Gar|k <garik.djan@gmail.com>
2025-12-10 21:59:07 +0300

    feat: add bme sensor support

--- firmware/espnow_sender/main/main.c
+++ firmware/espnow_sender/main/main.c
@@ -1,3 +1,5 @@
+#include "bme280.h"
+#include "driver/gpio.h"
 #include "esp_event.h"
 #include "esp_idf_version.h"
 #include "esp_log.h"
@@ -6,6 +8,7 @@
 #include "esp_now.h"
 #include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
+#include "i2c_bus.h"
 #include "nvs_flash.h"
 #include <assert.h>
 #include <stdlib.h>
@@ -14,6 +17,15 @@
 #define ESPNOW_WIFI_IF ESP_IF_WIFI_STA
 #define ESPNOW_QUEUE_SIZE 6
 #define ESPNOW_MAXDELAY 512
+
+#define BME_SDA GPIO_NUM_5        // GPIO 5 (SDA)
+#define BME_SCL GPIO_NUM_6        // GPIO 6 (SCL)
+#define BME_OUTPUT_PWR GPIO_NUM_4 // GPIO 4 (HIGH)
+
+#define I2C_MASTER_FREQ_HZ 100000
+#define I2C_MASTER_NUM I2C_NUM_0
+
+#define GPIO_OUTPUT_PIN_SEL ((1ULL << BME_OUTPUT_PWR))
 
 typedef enum {
     SEND_CB,
@@ -38,6 +50,9 @@ static const char *TAG = "esp_now_sender";
 static uint8_t s_mac[ESP_NOW_ETH_ALEN] = {0x02, 0x12, 0x34, 0x56, 0x78, 0x9A};
 static uint8_t s_peer_mac[ESP_NOW_ETH_ALEN] = {0xa0, 0xb7, 0x65, 0x15, 0x84, 0x45}; // a0:b7:65:15:84:45
 static QueueHandle_t s_event_queue = NULL;
+
+static i2c_bus_handle_t i2c_bus = NULL;
+static bme280_handle_t bme280 = NULL;
 
 /* WiFi should start before using ESPNOW */
 static void wifi_init(void) {
@@ -139,6 +154,79 @@ static esp_err_t test_espnow_init(void) {
     return ESP_OK;
 }
 
+static esp_err_t bme280_init() {
+    esp_err_t ret;
+
+    gpio_config_t io_conf = {.pin_bit_mask = GPIO_OUTPUT_PIN_SEL, .mode = GPIO_MODE_OUTPUT};
+    ret = gpio_config(&io_conf);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "gpio_config failed: %s", esp_err_to_name(ret));
+        return ret;
+    }
+
+    ret = gpio_set_level(BME_OUTPUT_PWR, 1);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "gpio_set_level failed: %s", esp_err_to_name(ret));
+        return ret;
+    }
+
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = BME_SDA,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = BME_SCL,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+    i2c_bus = i2c_bus_create(I2C_MASTER_NUM, &conf);
+    if (i2c_bus == NULL) {
+        ESP_LOGE(TAG, "i2c_bus_create failed");
+        return ESP_FAIL;
+    }
+
+    bme280 = bme280_create(i2c_bus, BME280_I2C_ADDRESS_DEFAULT);
+    if (bme280 == NULL) {
+        ESP_LOGE(TAG, "bme280_create failed");
+        return ESP_FAIL;
+    }
+
+    ret = bme280_default_init(bme280);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "bme280_default_init failed: %s", esp_err_to_name(ret));
+        return ret;
+    }
+
+    return ESP_OK;
+}
+
+static esp_err_t bme280_deinit() {
+    bme280_delete(&bme280);
+    i2c_bus_delete(&i2c_bus);
+
+    gpio_set_level(BME_OUTPUT_PWR, 0);
+}
+
+void bme280_test_getdata() {
+    int cnt = 10;
+    while (cnt--) {
+        float temperature = 0.0, humidity = 0.0, pressure = 0.0;
+        if (ESP_OK == bme280_read_temperature(bme280, &temperature)) {
+            ESP_LOGI(TAG, "temperature:%f ", temperature);
+        }
+        vTaskDelay(300 / portTICK_RATE_MS);
+        if (ESP_OK == bme280_read_humidity(bme280, &humidity)) {
+            ESP_LOGI(TAG, "humidity:%f ", humidity);
+        }
+        vTaskDelay(300 / portTICK_RATE_MS);
+        if (ESP_OK == bme280_read_pressure(bme280, &pressure)) {
+            ESP_LOGI(TAG, "pressure:%f\n", pressure);
+        }
+        vTaskDelay(300 / portTICK_RATE_MS);
+    }
+}
+
 void app_main(void) {
     // Initialize NVS
     esp_err_t ret = nvs_flash_init();
@@ -150,4 +238,11 @@ void app_main(void) {
 
     wifi_init();
     test_espnow_init();
+
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+    ESP_ERROR_CHECK(bme280_init());
+
+    bme280_test_getdata();
+    ESP_LOGI(TAG, "BME280 data read complete");
 }
