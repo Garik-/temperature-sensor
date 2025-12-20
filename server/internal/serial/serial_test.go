12dfb815b12234e7ae09227dfabca6b14e9235bb
Gar|k <garik.djan@gmail.com>
2025-12-20 19:38:24 +0300

    fix: test

--- server/internal/serial/serial_test.go
+++ server/internal/serial/serial_test.go
@@ -13,10 +13,12 @@ func TestParseString(t *testing.T) {
 		expected bool
 		payload  payload
 	}{
-		{"I (4041275) qf8mzr: 2314,2834,99819", true, payload{temperature: 2314, humidity: 2834, pressure: 99819}},
+		{"I (4041275) qf8mzr: 2314,2834,99819,32", true,
+			payload{temperature: 2314, humidity: 2834, pressure: 99819, voltage: 32}},
 		{"I (378) heap_init: At 3FFAE6E0 len 00001920 (6 KiB): DRAM", false, payload{}},
 		{"I (4041275) qf8mzr: 2314,2834", false, payload{}},
-		{"I (4041275) qf8mzr: 2314,2834,99819,123", true, payload{temperature: 2314, humidity: 2834, pressure: 99819}},
+		{"I (4041275) qf8mzr: 2314,2834,99819,123", true,
+			payload{temperature: 2314, humidity: 2834, pressure: 99819, voltage: 123}},
 		{"I (4041275) qf8mzr: abc,2834,99819", false, payload{}},
 		{"Random string without tag", false, payload{}},
 	}
